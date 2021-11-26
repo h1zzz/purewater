b6ba7ee8976c7cabce0b179030eacc1c5c44ce3d
h1zzz <24761228+h1zzz@users.noreply.github.com>
2021-11-26 18:16:29 +0800

    delete empty file

--- agent/conn_pool.c
+++
@@ -1,4 +0,0 @@
-/* MIT License Copyright (c) 2021, h1zzz */
-
-#include "conn_pool.h"
-
--- agent/event.h
+++
@@ -1,6 +0,0 @@
-/* MIT License Copyright (c) 2021, h1zzz */
-
-#ifndef _EVENT_H
-#define _EVENT_H
-
-#endif /* event.h */
