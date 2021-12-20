a5aefa4fb03246681fafc7daa20c70ca3a80cca6
h1zzz <24761228+h1zzz@users.noreply.github.com>
2021-12-20 18:34:10 +0800

    add sleep

--- agent/util.h
+++ agent/util.h
@@ -3,6 +3,18 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
+#ifdef _WIN32
+#include <windows.h>
+#else /* No define _WIN32 */
+#include <unistd.h>
+#endif /* _WIN32 */
+
+#ifdef _WIN32
+#define xsleep(x) Sleep((x) * 1000)
+#else  /* No define _WIN32 */
+#define xsleep(x) sleep(x)
+#endif /* _WIN32 */
+
 const char *xbasename(const char *str);
 int xrand(void);
 char *xstrdup(const char *str);
