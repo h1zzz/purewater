3742062fad3f2e14de473ed45df85cdd7f0d8a61
h1zzz <24761228+h1zzz@users.noreply.github.com>
2022-01-05 15:13:56 +0800

    update

--- agent/debug.h
+++ agent/debug.h
@@ -13,16 +13,21 @@
 #endif /* NDEBUG */
 
 #ifdef NDEBUG
+
 #define assert(expr)
 #define debug(str)
 #define debugf(fmt, ...)
+
 #else /* No define NDEBUG */
+
 #define debug(str)                                                             \
     fprintf(stdout, "\033[1;90m[%s:%d] \033[0m" str "\n", xbasename(__FILE__), \
             __LINE__);
+
 #define debugf(fmt, ...)                                                       \
     fprintf(stdout, "\033[1;90m[%s:%d] \033[0m" fmt "\n", xbasename(__FILE__), \
             __LINE__, __VA_ARGS__);
+
 #endif /* NDEBUG */
 
 #endif /* debug.h */
