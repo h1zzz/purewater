b2a92e2a0f9cd8b1335009346b19bf82f5e94b70
h1zzz <24761228+h1zzz@users.noreply.github.com>
2022-03-21 14:00:27 +0800

    update

--- desktop/src/main.js
+++ desktop/src/main.js
@@ -4,7 +4,7 @@
 
 const { app, BrowserWindow } = require('electron');
 
-const createWindow = function() {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 500,
@@ -12,12 +12,13 @@ const createWindow = function() {
 }
 
 app.on('ready', function() {
-  createWindow()
-  app.on('activate', function() {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+  createWindow();
+});
+
+app.on('activate', function() {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 app.on('window-all-closed', function() {
